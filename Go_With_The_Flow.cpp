db4f546e3ad40f1efb161ec1728172cb558eb156
simon1daccache <52183340+simon1daccache@users.noreply.github.com>
2019-07-10 23:41:41 +0300

    Update Go_With_The_Flow.cpp

--- Go_With_The_Flow.cpp
+++ Go_With_The_Flow.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
+    //variable declaration
+    int n; //number of words
+    string paragraph; // paragraph input
+    char **P; //pointer to get the paragraph into an array
 
+    //get the number n of words
+    do
+    {
+        cin>>n;
+
+    }while (n<2||n>2500);
+
+    cin>> paragraph;
+
+    
+return 0;
 }
