ddaf027972f1027112983d2c5631fe56bbc1a4b4
simon1daccache <simon1daccache@gmail.com>
2019-07-11 11:15:41 +0300

    added function to get the longest length of the word

--- Go_With_The_Flow.cpp
+++ Go_With_The_Flow.cpp
@@ -3,13 +3,62 @@
 
 using namespace std;
 
+ struct Node
+    {
+        string data;
+
+        struct Node *next;
+        struct Node *prev;
+
+    };
+struct Node* head= NULL;
+
+void insert_data(string new_data)
+{
+  struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+   new_node->data = new_data; 
+   new_node->next = head; 
+   head = new_node;
+}
+
+
+//function to search for the longest word
+string longest_word(string paragraph)
+{
+    int index=0;
+    int longest_length=1;
+
+    for (int i = 0; i < paragraph.length; i++)
+    {
+        if (paragraph[i] != ' ')
+        {
+            index++ ;
+        }
+        else if(paragraph[i] == ' ')
+        {
+            if(longest_length<index)
+            {
+                 longest_length = index;
+                 index = 0;
+            }
+            
+        }
+    }
+}
+
+
+
+
+
 int main()
 {
     //variable declaration
     int n; //number of words
     string paragraph; // paragraph input
-    char **P; //pointer to get the paragraph into an array
-
+    string big_word;    
+   
+    
+    
     //get the number n of words
     do
     {
@@ -18,16 +67,18 @@ int main()
     }while (n<2||n>2500);
 
     cin>> paragraph;
-    
-    //for loop with counter i to enter the number
-    for(int i=0;i<n;i++)
+
+  
+    for(int i=0;i<n;i++) //maximum value of rows that the array can reach is the total number of letters -1
     {
-        for(int j=0;j<n;j++)
-        {
-            
-        }
+       
     }
 
+    //loop to get the different line width and the longest river
+    for(int i=big_word.length;i<80;i++)
+    {
+        
+    }
     
 return 0;
 }
